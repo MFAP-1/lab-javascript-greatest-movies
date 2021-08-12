f6de281b4ad511b7b66f847ae1c5e5936c099ce3
Manoel Pereira <manoelpereiraeng@gmail.com>
2021-08-12 20:39:33 -0300

    2nd commit. Lab solved (bonus included).

--- src/movies.js
+++ src/movies.js
@@ -24,6 +24,7 @@ function howManyMovies(movies) {
   return filteredArr.length; 
 }
 
+
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(movies) {
     if (movies.length === 0) return 0; // checking if the array is empty. If it is, returns 0.
@@ -48,6 +49,7 @@ function dramaMoviesScore(movies) {
   return parseFloat((sum / dramaCount).toFixed(2)); //rounded to 2 decimals places
 }
 
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(movies) {
   if (movies.length === 0) return []; // checking if the array is empty. If it is, returns [].
@@ -57,15 +59,14 @@ function orderByYear(movies) {
   });
 }
 
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(movies) {
-  //if (movies.length === 0) return []; // checking if the array is empty. If it is, returns [].
-  const backup = movies;
-  const moviesOrdered = backup.sort((a, b) => {
+  const moviesOrdered = [...movies].sort((a, b) => { // spread-syntax of the original array to avoid mutating
     return a.title.localeCompare(b.title);
   });
   const top20Aplha = []; // new empty array, for storing only the top 20 titles by Alphabetic Order
-  moviesOrdered.map((element, index) => {
+  moviesOrdered.map((element, index) => { // mapping the newly ordered complet array
     if (index < 20) { // 20 for the 0-19's elements, thus max 20.
       top20Aplha.push(element.title);
      }
@@ -73,13 +74,90 @@ function orderAlphabetically(movies) {
   return top20Aplha;
 }
 
+
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
+function turnHoursToMinutes(movies) {
+  /* The commented section below was trying once again to fix the test 'should return a new array, 
+    not update the original one'. */
+    
+  // const convertedMovies = [...movies];
+  // convertedMovies.map(element => {
+  //   // Series of 'if statements' verifying the length of the duration string
+  //   if (element.duration.length > 7){  // Treating the case formatted with 'Xh XX min'
+  //     let hours = parseInt(element.duration.split('h ')[0]); // obtaining the amount of hours
+  //     let minutes = parseInt(element.duration.split('h ')[1].split('min')[0]); // obtaining the amount of minutes
+  //     element.duration = (hours * 60 + minutes);
+  //   } else if (element.duration.length === 2) { // Treating the case formatted with 'Xh'
+  //     let hours = parseInt(element.duration.split('h ')[0]); // obtaining the amount of hours
+  //     element.duration = (hours * 60);
+  //   } else { // Treating the case with no duration assigned
+  //     element.duration = 0;
+  //   }
+  // });
+  // return convertedMovies;
+
+  
+  // Actual solution (pass all tests)
+  let convertedMovies = [];
+  for (let i = 0; i < movies.length; i++) {
+    // The i'th element of the convertedMovies reciving the i'th element from movie. Before editing
+    convertedMovies[i] = {
+      title: movies[i].title,
+      year: movies[i].year,
+      director: movies[i].director,
+      duration: movies[i].duration,
+      genre: movies[i].genre,
+      score: movies[i].score
+    }
+    // Series of 'if statements' verifying the length of the duration string
+    if (convertedMovies[i].duration.length > 7){  // Treating the case formatted with 'Xh XX min'
+      let hours = parseInt(convertedMovies[i].duration.split('h ')[0]); // obtaining the amount of hours
+      let minutes = parseInt(convertedMovies[i].duration.split('h ')[1].split('min')[0]); // obtaining the amount of minutes
+      convertedMovies[i].duration = (hours * 60 + minutes);
+    } else if (convertedMovies[i].duration.length === 2) { // Treating the case formatted with 'Xh'
+      let hours = parseInt(convertedMovies[i].duration.split('h ')[0]); // obtaining the amount of hours
+      convertedMovies[i].duration = (hours * 60);
+    } else { // Treating the case with no duration assigned
+      convertedMovies[i].duration = 0;
+    }
+  }
+  return convertedMovies;
+}
+
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() {}
-
-
+function bestYearAvg(movies) {
+  if (movies.length === 0) return null; // checking if the array is empty. If it is, returns null.
+  let obj = { // object to help treat the data
+    year: [], // array with all years in number format
+    sumScores: [], // sum of scores in that year
+    count: [] // array with the count of movies in a given year
+  };
+  // Maping the whole movies array to an object of arrays
+  movies.map(element => {
+    if (obj.year.indexOf(element.year) === -1) { // in case the year wasnt add yet
+      obj.year.push(element.year);
+      obj.sumScores.push(element.score);
+      obj.count.push(1);
+    } else { // when the current element's year was previously added
+      curentObjIndex = obj.year.indexOf(element.year);
+      obj.sumScores[curentObjIndex] += element.score;
+      obj.count[curentObjIndex]++;
+    }
+  });
+  // Evaluating the final results of the obj
+  let topYear = 0;
+  let topAvgScore = 0;
+  obj.year.map((element, index) => {
+    if ((obj.sumScores[index] / obj.count[index]) > topAvgScore) {
+        topYear = element;
+        topAvgScore = (obj.sumScores[index] / obj.count[index]);
+    } else if ((obj.sumScores[index] / obj.count[index]) === topAvgScore) { // in case of a tie
+      if (topYear > element) topYear = element; // the topYear will only be updated for the older one
+    }
+  });
+  return `The best year was ${topYear} with an average score of ${topAvgScore}`;
+}
 
 
 
